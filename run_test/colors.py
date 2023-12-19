6389e7168fd516c76babd126363b5eaa27d2ac21
katarzyna-da-silva <conic916@gmail.com>
2023-12-19 14:21:27 +0100

    add colors and test unit py

---
+++ run_test/colors.py
@@ -0,0 +1,36 @@
+# script.py
+
+class Colors:
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    # Foreground colors
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+
+    # Background colors
+    BLACK_BG = '\033[40m'
+    RED_BG = '\033[41m'
+    GREEN_BG = '\033[42m'
+    YELLOW_BG = '\033[43m'
+    BLUE_BG = '\033[44m'
+    MAGENTA_BG = '\033[45m'
+    CYAN_BG = '\033[46m'
+    WHITE_BG = '\033[47m'
+
+def colorize_text(text, color_code):
+    return f"{color_code}{text}{Colors.RESET}"
+
+# Tworzenie napisu po francusku
+text = "Je serai un devOps bientot ou pas! ;)"
+
+# Kolorowanie i wyświetlanie napisu
+colored_text = colorize_text(text, f"{Colors.BOLD}{Colors.GREEN}")
+print(colored_text)
